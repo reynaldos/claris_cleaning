97b318c404597622bca6292266595cb10558c670
reynaldos <reyjr98@gmail.com>
2024-02-20 18:26:24 -0500

    metadata fixes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,11 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
-import ClientProviders from '@/state/providers'
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import { BodyWrap } from './layout.styles'
-
+import ClientProviders from "@/state/providers";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import { BodyWrap } from "./layout.styles";
 
 const openSans = Inter({ subsets: ["latin"] });
 
@@ -14,34 +13,48 @@ export const metadata: Metadata = {
   title: "Clari's Cleaning Crew",
   description:
     "Cleaning Services: Contact Clari's Cleaning Company for the best in residential, commercial, and construction cleaning services.",
-  icons: [
-    {
-      sizes: "180x180",
-      url: "./logo/ccc_logo_180x180.png",
-      type: "image/png",
-    },
-    { sizes: "32x32", url: "./logo/ccc_logo_32x32.png", type: "image/png" },
-    { sizes: "16x16", url: "./logo/ccc_logo_16x16.png", type: "image/png" },
-  ],
-  
-  openGraph: {
-    title: "Clari's Cleaning Crew",
-    images:[{url: "./assets/share.png"}],
-    description:
-      "Cleaning Services: Contact Clari's Cleaning Company for the best in residential, commercial, and construction cleaning services.",
-  },
-  appleWebApp: {
-    capable: true,
-    statusBarStyle: "black-translucent",
-  },
 };
 
 <link rel="icon" href="/favicon.ico" sizes="any" />;
+<meta
+  name="viewport"
+  content="width=device-width, initial-scale=1, shrink-to-fit=no"
+/>;
+
+<meta name="theme-color" content="#000000" />;
+
+<meta property="og:title" content="Clari's Cleaning Crew" />;
+<meta property="og:image" content="./assets/share.png" />;
+<meta
+  property="og:description"
+  content="Cleaning Services: Contact Clari's Cleaning Company for the best in residential, commercial, and construction cleaning services."
+/>;
+
+<link rel="shortcut icon" sizes="180x180" href="./logo/ccc_logo_180x180.png" />;
+<link
+  rel="icon"
+  type="image/png"
+  sizes="32x32"
+  href="./logo/ccc_logo_32x32.png"
+/>;
+<link
+  rel="icon"
+  type="image/png"
+  sizes="16x16"
+  href="./assets/logo/logo-16x16.png"
+/>;
+<link
+  rel="apple-touch-icon"
+  sizes="180x180"
+  href="./logo/ccc_logo_16x16.png"
+/>;
+
+<meta name="apple-mobile-web-app-capable" content="yes" />;
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -49,30 +62,11 @@ export default function RootLayout({
         <ClientProviders>
           <Navbar />
 
-          <BodyWrap className='bodyWrap'>
-             {children}
-          </BodyWrap>
-          
+          <BodyWrap className="bodyWrap">{children}</BodyWrap>
+
           <Footer />
         </ClientProviders>
       </body>
     </html>
   );
 }
-
-
-// Keller Williams Realty Lakeland
-
-// FHS/Mazzella 
-
-// SouthState Bank Bartow
-
-// Henry Company
-
-// Firestone Lakeland
-
-// Better Earth 
-
-// Children and Teen Dental, Lakeland and Winter Haven
-
-// https://www.melaleuca.com/productstore/cleaning-and-laundry
