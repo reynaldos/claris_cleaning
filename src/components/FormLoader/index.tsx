e8e52687da9087e096a39e37da698df3d5b5898a
reynaldos <reyjr98@gmail.com>
2024-03-15 14:37:08 -0400

    contact form implemented

---
+++ src/components/FormLoader/index.tsx
@@ -0,0 +1,56 @@
+"use client"
+
+import React, { useEffect } from "react";
+import { Container, Label, Square, Wrapper } from "./FormLoader.styles";
+
+
+export enum EmailStateEnum {
+  SENT = "sent",
+  IDLE = "idle",
+  LOADING = "loading",
+  ERROR = "error",
+}
+
+
+const FormLoader = ({
+  email,
+}: {
+  email: { emailState: EmailStateEnum; setEmailState: any };
+}) => {
+  const { emailState, setEmailState } = email;
+
+  useEffect(() => {
+    if (
+      emailState === EmailStateEnum.SENT ||
+      emailState === EmailStateEnum.ERROR
+    )
+      setTimeout(() => {
+        setEmailState(EmailStateEnum.IDLE);
+      }, 2000);
+  }, [emailState]);
+
+  return (
+    <Container>
+      {emailState === EmailStateEnum.LOADING ? (
+        <Wrapper>
+          <Square id="sq1" />
+          <Square id="sq2" />
+          <Square id="sq3" />
+          <Square id="sq4" />
+          <Square id="sq5" />
+          <Square id="sq6" />
+          <Square id="sq7" />
+          <Square id="sq8" />
+          <Square id="sq9" />
+        </Wrapper>
+      ) : (
+        <Label>
+          {<h2>{emailState === EmailStateEnum.ERROR ? <>Error, try again.</>: <>Thank you for reaching out,<br/>we will contact you shortly!</>}</h2>}
+        </Label>
+      )}
+    </Container>
+  );
+};
+
+
+export default FormLoader;
--- src/state/providers.tsx
+++ src/state/providers.tsx
@@ -4,12 +4,34 @@ import { ThemeProvider } from "styled-components";
 
 import StyledComponentsRegistry from "@/lib/registry";
 import { globalTheme } from "../constants/theme";
+import { useEffect } from "react";
+
+import emailjs from "@emailjs/browser";
+
+const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY || "";
 
 export default function ClientProviders({
   children,
 }: {
   children: React.ReactNode;
 }) {
+
+  useEffect(() => {
+      emailjs.init({
+        publicKey: publicKey,
+        // Do not allow headless browsers
+        blockHeadless: true,
+        limitRate: {
+          // Set the limit rate for the application
+          id: "app",
+          // Allow 1 request per 10s
+          throttle: 10000,
+        },
+      });
+  }, [])
+  
+
+
   return  (
      <StyledComponentsRegistry>
        <ThemeProvider theme={globalTheme}>{children}</ThemeProvider>
